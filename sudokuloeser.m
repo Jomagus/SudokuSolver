3df357468a111ce97f854993b0fd23f7b029ec62
Jomagus <github@jomagus.de>
2015-12-06 13:20:18 +0100

    Translated and commented a bit

--- sudokuloeser.m
+++ sudokuloeser.m
@@ -1,18 +1,23 @@
 function S = sudokuloeser(S)
+% SUDOKULOESER  Solves a given NxN Sudoku supplied as a Matrix, where missing fields are "0".
 
-    %Hier schauen wir uns eindeutige Loesungen an    
+    % First we check if we dont need guesses to solve the Sudoku   
 
-    S = eindeutig(S);
+    S = unique(S);
         
     if issolved(S)
         return
     end
     
-    %Hier beginnt der eigentliche Loeser
+    % Now we solve the Sudoku with Backtracking. If there are multiple possible numbers,
+    % we choose one and see, if we can solve the Sudoku, if it fails we backtrack and 
+    % look at the other possible numbers.
     
-    [S, succes] = annehmer(S,1,1);
+    [S, success] = accepter(S,1,1);
     
-    if succes == 1
+    % If the success flag is set, our Sudoku is solved, otherwise there is no possible solution.
+
+    if success == 1
         return;
     else
         S=[];
@@ -22,8 +27,28 @@ end
 
 
 function [flag] = sudokutester(K,i,j,z)
+% SUDOKUTESTER  Checks if a number is allowed in a given place.
+%   a = sudokutester(K,i,j,z) checks if z is allowed to be placed on position (i,j) in the Sudoku K, if so, returns 1, otherwise 0.
+
 if (K(i,j) == 0) && (isempty(find(K(i,:) == z, 1))) && (isempty(find(K(:,j) == z, 1)))
     
+    % Our Sudukos have the Rule, that every number in a subsquare must be unique, meaning for a 3x3 Sudoku:
+    %
+    % A A A | B B B | C C C
+    % A A A | B B B | C C C
+    % A A A | B B B | C C C
+    %----------------------
+    % D D D | E E E | F F F
+    % D D D | E E E | F F F
+    % D D D | E E E | F F F
+    %----------------------
+    % G G G | H H H | I I I
+    % G G G | H H H | I I I
+    % G G G | H H H | I I I
+    %
+    % that for each subsquare A-I there cant be a number twice in it
+
+
     SubSize=sqrt(size(K));
     Subi=fix((i-1)/SubSize(1));
     Subj=fix((j-1)/SubSize(2));
@@ -46,9 +71,13 @@ end
 end
 
 function PosSol = Solutions(K,istart,jstart)
+% POSSOL Returns a 3 dimensional Matrix that contains all possible numbers for a given sudoku.
+% In the return Matrix the value on position (i,j,k) is 1 only if its allowed to place the number
+% k on position (i,j) of the sudoku (that is supplied to this function).
+
 PosSolT = zeros(size((K),1),size((K),1),size((K),1));
 
-for d3n = 1:size((K),1) %läuft durch alle möglichen Zahlen
+for d3n = 1:size((K),1) % runs over all possible numbers
     for d1n = istart:size((K),1)
         for d2n = jstart:size((K),1)
             PosSolT(d1n,d2n,d3n)= sudokutester(K,d1n,d2n,d3n);
@@ -58,16 +87,18 @@ end
 PosSol = PosSolT;
 end
 
-function K = eindeutig(K)
+function K = unique(K)
+% UNIQUE Fills all numbers in the sudoku that are possible without backtracking (meaning that there is alway only 1 choice to make)
+
     while 1
-        KCopy = eindeutighelper(K);
+        KCopy = uniquehelper(K);
         if isequal(K,KCopy)
             break
         end
         K = KCopy;
     end
 
-    function K = eindeutighelper(K)
+    function K = uniquehelper(K)
         PosSol = Solutions(K,1,1);
 
         for di = 1:size((K),1)
@@ -81,6 +112,8 @@ function K = eindeutig(K)
 end
 
 function flag = issolved(K)
+% ISSOLVED Checks if supplied sudoku is solved.
+
     if isempty(find(K==0,1))
         HMat = zeros(size(K,1));
         for di = 1:size(K,1)
@@ -126,7 +159,9 @@ function flag = issolved(K)
     end
 end
 
-function [ZwL, succes] = annehmer(K,curi,curj)
+function [ZwL, success] = accepter(K,curi,curj)
+% ACCEPTER Trys so solve the sudoku with backtracking.
+
     akti=curi;
     aktj=curj;
     
@@ -141,9 +176,9 @@ function [ZwL, succes] = annehmer(K,curi,curj)
             if aktj > size(K,1)  %falls ganz am Ende
                ZwL = K;
                if issolved(K)
-                    succes = 1;
+                    success = 1;
                else
-                   succes = 0;
+                   success = 0;
                end
                return
             end
@@ -154,34 +189,27 @@ function [ZwL, succes] = annehmer(K,curi,curj)
     end
     
     PosSol = Solutions(K,akti,aktj);    
-    moeglich = find(PosSol(akti,aktj,:) == 1);   %gibt alle Moeglichkeiten an
+    possible = find(PosSol(akti,aktj,:) == 1);   % gives all possibilities
       
-    KCopy = K;              %sichert vor dem Testen
+    KCopy = K;              % to save K before testing
     
-    for assumed = 1:length(moeglich)
-        K = KCopy;          %resettet K vor jedem durchlauf ohen erfolg
-        K(akti,aktj)=moeglich(assumed);
-        K = eindeutig(K);
+    for assumed = 1:length(possible)
+        K = KCopy;          %resettet K before each unsuccessful attempt
+        K(akti,aktj)=possible(assumed);
+        K = unique(K);
         if issolved(K)
             ZwL = K;
-            succes = 1;
+            success = 1;
             return
         end
         
-        [KNew, succes] = annehmer(K,akti,aktj);
-        if succes == 1
+        [KNew, success] = accepter(K,akti,aktj);
+        if success == 1
             ZwL = KNew;
             return
         end
     end
     
     ZwL=KCopy;
-    succes=0;
+    success=0;
 end
-
-
-
-
-
-
-
